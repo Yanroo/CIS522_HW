5d91e40ab961ec95cce56b6568dfb3c703bf7960
YanroV5 <yanro125996@gmail.com>
2023-02-20 10:43:30 -0500

    epoch13

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -7,7 +7,7 @@ from torchvision.transforms import Compose, ToTensor
 
 class CONFIG:
     batch_size = 32
-    num_epochs = 19
+    num_epochs = 13
     initial_learning_rate = 0.003
     initial_weight_decay = 0
 
