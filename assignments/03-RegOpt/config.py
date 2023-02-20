b97628082598e3dff86b6b27ca01eb217a23aa9b
YanroV5 <yanro125996@gmail.com>
2023-02-20 01:15:01 -0500

    epoch20

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -7,7 +7,7 @@ from torchvision.transforms import Compose, ToTensor
 
 class CONFIG:
     batch_size = 32
-    num_epochs = 13
+    num_epochs = 20
     initial_learning_rate = 0.003
     initial_weight_decay = 0
 
@@ -15,7 +15,7 @@ class CONFIG:
         # You can pass arguments to the learning rate scheduler
         # constructor here.
         "step_size": 0.2 * num_epochs * 50000 // batch_size,
-        "verbose": True,
+        "verbose": False,
         "gamma": 0.5,
     }
 
