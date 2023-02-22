e1673a5b8d4f019a40e4a15a4c012bc4b0a6b8a2
YanroV5 <yanro125996@gmail.com>
2023-02-21 22:19:19 -0500

    15epoch32_normal

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -7,7 +7,7 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 class CONFIG:
     batch_size = 32
-    num_epochs = 14
+    num_epochs = 15
     initial_learning_rate = 0.002
     initial_weight_decay = 0
 
