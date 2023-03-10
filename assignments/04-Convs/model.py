217466beb2979a1c06748ae3e04a6d09d8bee363
YanroV5 <yanro125996@gmail.com>
2023-03-10 18:09:00 -0500

    32_128_batch32

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,29 @@
 import torch
+from torch import nn
+import torch.nn.functional as F
 
 
-class Model(torch.nn.Module):
+class Model(nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        super(Model, self).__init__()
+        out_dim1 = 32
+        out_dim2 = 64
+        self.conv1 = nn.Conv2d(num_channels, out_dim1, 3, 1, padding="same")
+        # self.conv2 = nn.Conv2d(32, 64, 3, 1, padding="same")
+        self.fc1 = nn.Linear(out_dim1 * 32 * 32 // 4, 128)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        x = self.conv1(x)
+        x = F.relu(x)
+        # x = self.conv2(x)
+        # x = F.relu(x)
+        # print("shape1: ", x.shape)
+        x = F.max_pool2d(x, 2)
+        # print("shape2: ", x.shape)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+
+        return x
