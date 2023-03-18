566b5b706b85101a261949d5054fed2cec9a34eb
YanroV5 <yanro125996@gmail.com>
2023-03-18 11:38:11 -0400

    c10_p4_b128_same

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -10,30 +10,43 @@ class Model(nn.Module):
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
-        out_dim1 = 20
+        cout_dim1 = 10
         # out_dim2 = 32
         fout_dim1 = 16
-        self.conv1 = nn.Conv2d(num_channels, out_dim1, 3, 1, padding="same")
+        # fout_dim2 = 16
+        # self.conv1_dw = nn.Conv2d(num_channels, num_channels, 3, 1, padding="same", groups=num_channels)
+        # self.conv1_pw = nn.Conv2d(num_channels, out_dim1, 1, 1)
+
+        self.conv1 = nn.Conv2d(num_channels, cout_dim1, 3, 1, padding="same")
         # self.conv2 = nn.Conv2d(out_dim1, out_dim2, 1, 1, padding="same")#
-        # self.fc1 = nn.Linear(out_dim1 * 32 * 32 // 4, fout_dim1)
-        self.fc1 = nn.Linear(1280, fout_dim1)
-        self.fc2 = nn.Linear(fout_dim1, num_classes)
+        self.fc1 = nn.Linear(cout_dim1 * 32 * 32 // 16, num_classes)
+        # self.fc2 = nn.Linear(fout_dim1, num_classes)
+        # self.fc1 = nn.Linear(960, fout_dim1)
+        # self.bn1 = nn.BatchNorm1d(fout_dim1)
+        # self.fc3 = nn.Linear(fout_dim2, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Forward pass of the model.
         """
+
         x = self.conv1(x)
+        # x = self.conv1_dw(x)
+        # x = self.conv1_pw(x)
         # x = F.relu(x)
         # x = self.conv2(x)
         # x = F.relu(x)
         # print("shape1: ", x.shape)
         x = F.max_pool2d(x, 4)
-        # x = F.relu(x)
         # print("shape2: ", x.shape)
         x = torch.flatten(x, 1)
+        # x = F.max_pool1d(x, 2)
         x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
+        # x = self.bn1(x)
+        # x = F.relu(x)
+
+        # x = self.fc2(x)
+        # x = F.relu(x)
+        # x = self.fc3(x)
 
         return x
