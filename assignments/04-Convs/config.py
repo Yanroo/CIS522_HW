ba32d70eddebd43837f2ed9814fe803da865b51e
YanroV5 <yanro125996@gmail.com>
2023-03-18 12:06:32 -0400

    c14_p4_b128

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -12,9 +12,8 @@ class CONFIG:
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(model.parameters(), lr=3e-3)
-    
-    #torch.optim.SGD(model.parameters(), lr=8e-2, momentum=0.9)
-    
+
+    # torch.optim.SGD(model.parameters(), lr=8e-2, momentum=0.9)
 
     transforms = Compose(
         [ToTensor(), Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))]
